2dbbcc88305bbe426e681c59768ec9952565e616
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-13 23:36:07 +0300

    slug

--- app/portfolio/[slug]/page.js
+++ app/portfolio/[slug]/page.js
@@ -1,24 +1,24 @@
-// import { teams  } from "./teams"
-// export default function Portofolio ({params}) {
-//     let info
-//     console.log("test test")
-//     teams.map(team => {
-//         if(team.name === params.slug){
-//             info = team
-//         }
-//     })
-//     return (
-//         <>
-//         <h1>djfjksdhkfhkqshdfkhqkhfkqsjh {info.fname}</h1>
-//        </>
-//     )
+import { teams  } from "./teams"
+export default function Portofolio ({params}) {
+    let info
+    console.log("test test")
+    teams.map(team => {
+        if(team.name === params.slug){
+            info = team
+        }
+    })
+    return (
+        <>
+        <h1>djfjksdhkfhkqshdfkhqkhfkqsjh {info.fname}</h1>
+       </>
+    )
         
-// }
+}
 
-// export async function generateStaticParams() {
+export async function generateStaticParams() {
 
-//       return  teams.map(team => ({
-//            slug: team.name
-//       }))
+      return  teams.map(team => ({
+           slug: team.name
+      }))
 
-// }
+}
