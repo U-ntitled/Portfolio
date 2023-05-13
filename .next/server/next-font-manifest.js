8dfdc319960e05fac7eaec84f0b5a6405df2cf1e
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-05-13 22:33:45 +0300

    project structure

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,8 @@
+self.__NEXT_FONT_MANIFEST={
+  "pages": {},
+  "app": {
+    "/media/riry7474/94484ECF484EAFB2/$WINDOWS.~BT/PROJECTS/UNTILED/Portfolio/node_modules/next/font/google/target.css?{\"path\":\"app/layout.js\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}": []
+  },
+  "appUsingSizeAdjust": true,
+  "pagesUsingSizeAdjust": false
+}
---
+++ app/portfolio/[slug]/page.js
@@ -0,0 +1,24 @@
+// import { teams  } from "./teams"
+// export default function Portofolio ({params}) {
+//     let info
+//     console.log("test test")
+//     teams.map(team => {
+//         if(team.name === params.slug){
+//             info = team
+//         }
+//     })
+//     return (
+//         <>
+//         <h1>djfjksdhkfhkqshdfkhqkhfkqsjh {info.fname}</h1>
+//        </>
+//     )
+        
+// }
+
+// export async function generateStaticParams() {
+
+//       return  teams.map(team => ({
+//            slug: team.name
+//       }))
+
+// }
