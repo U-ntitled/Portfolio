7700780ea26b98465ad0c5b55f0b8976453403dd
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-05-13 23:31:16 +0300

    project structure

---
+++ app/portfolio/[slug]/teams.js
@@ -0,0 +1,24 @@
+export const teams=[
+    {
+        name:"nomena",
+        fname:"Nomena"
+        
+    },
+    {
+        name:"riry",
+        fname:"Riry"
+        
+    },
+    {
+        name:"wendi",
+        fname:"Wendi"
+    },
+    {
+        name:"fiaro",
+        fname:"Fiaro"
+    },
+    {
+        name:"antoninoh",
+        fname:"Antoninoh"
+    }
+]
