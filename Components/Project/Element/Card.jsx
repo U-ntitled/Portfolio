3a8cb70c80a1f3028ae690ca16892b6781849c7f
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-14 12:04:50 +0300

    correction project section

--- Components/Project/Element/Card.jsx
+++ Components/Project/Element/Card.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Card({capture}) {
   return (
-    <div  className='bg-[#140D0D] h-[392px] w-[430px] relative flex flex-col items-center rounded-xl mb-2'>
+    <div  className='bg-[#140D0D] h-[392px] w-[430px] relative flex flex-col items-center rounded-xl'>
         <img src={capture}  className='p-3 h-[304px]' alt='enilab capture'/>
         <div className="flex justify-between items-center px-5 bg-[#282323] absolute bottom-0 w-full h-[84px]  rounded-br-xl rounded-bl-xl">
             <img src='info.png' alt='info' />
