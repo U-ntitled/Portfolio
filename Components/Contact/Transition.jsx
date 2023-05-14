25a43f0f3e70e9ba51d7a8c58ea7449c6216c4c8
PyWendi <rakotondranaivogilbert21@gmail.com>
2023-05-14 14:23:38 +0300

    Contact section

--- Components/Contact/Transition.jsx
+++ Components/Contact/Transition.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './style.module.css'
 
 function Transition() {
   return (
-    <div>Transition</div>
+    <div id="contact_section" className="">
+        <div className="text_section">
+            <div className="title_text">
+                <h2>LET'S CREATE <span className="green">SOMETHING AMAZING</span> TOGETHER</h2>
+            </div>
+            <p className="text">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eveniet nobis perspiciatis ab ipsa. 
+            </p>
+
+            <div className="purple_button">
+                <a href="#">GET IN TOUCH WITH US</a>
+            </div>
+        </div>
+
+
+        <div className="image_representation">
+            <img src="Saly-31 1.svg" alt="Communication illustration"/>
+        </div>
+    </div>    
   )
 }
 
