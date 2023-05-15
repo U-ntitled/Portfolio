0d6193eb503b37c3566f2aa19bd1c417c353a849
TiaDev7474 <nomenjanaharyriry@gmail.com>
2023-05-15 10:23:07 +0300

    hero section updates

--- Components/Hero/Hero.jsx
+++ Components/Hero/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
                 <div className='w-[50%] lg:flex-1 p-2'>
                      <img  src='LOGO.svg' alt='our logo' style={{width:220, height:100}}/>
                 </div>
-                <ul className='hidden md:flex-1  text-[16px] gap-3 md:flex  md:text-[18px] lg:gap-7 lg:text-[20px]' >
+                <ul className='hidden text-[16px]  md:flex-1 gap-3 md:flex  md:text-[18px] lg:gap-7 lg:text-[20px]' >
                     <li>
                       <a href='#'>Home</a>
                     </li>
@@ -26,8 +26,16 @@ function Hero() {
                     <a href='#contact' className='inline-block py-2 px-3 bg-btnprimary mt-2  cursor-pointer'>CONTACT US</a>
                 </div>
            </nav>
+           <div>
+                  <span className='absolute left-[40%] z-0'>
+                      <img src='blendToDown.svg' alt='graphique element' />
+                  </span>
+                  <span className='absolute bottom-0 right-0'>
+                      <img src='blendToUp.svg'  alt='graphique element' />
+                  </span>
+           </div>
            <div className=' w-[95%] h-[75vh]  flex justify-between  m-auto md:w-[80%] lg:w-[85%]'>
-                 <div className=' flex-1 flex justify-center'>
+                 <div className=' flex-1 flex justify-center z-10'>
                     <div className=' w-full mt-24 p-4 lg:w-[85%]'>
                         <h1 className=' w-[15ch] text-[36px] md:text-[64px]  leading-tight font-bold  md:py-5 text-white capitalize'>
                           We build <span className='bg-btnprimary'>innovative</span> Software Solutions</h1>
