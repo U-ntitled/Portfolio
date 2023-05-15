24a25cd6f8d0a60b186e916ef6798f80f6474e29
niriantsoa <niriantsoa@pop-os.localdomain>
2023-05-15 11:20:53 +0300

    Responsive team section

--- Components/Team/Team.jsx
+++ Components/Team/Team.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import './style.css'
+import './style.module.css'
 
 function Team() {
   const members = [
@@ -8,82 +8,82 @@ function Team() {
       fullName: 'RAKOTONANDRASANA Jean Pierre',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'upLeft bg-[#F0F0F0] flex flex-col m-4 rounded-xl items-center justify-evenly'
+      classes:'upLeft bg-[#F0F0F0] flex flex-col m-6 rounded-xl items-center justify-evenly'
     },
     {
       avatarUrl: '',
       fullName: 'RAKOTONANDRASANA Jean Pierre',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'center bg-[#F0F0F0] flex flex-col m-4 rounded-xl items-center justify-evenly'
+      classes:'center bg-[#F0F0F0] flex flex-col m-6 rounded-xl items-center justify-evenly'
     },
     {
       avatarUrl: '',
       fullName: 'RAKOTONANDRASANA Jean Pierre',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'upRight bg-[#F0F0F0] flex flex-col m-4 rounded-xl items-center justify-evenly'
+      classes:'upRight bg-[#F0F0F0] flex flex-col m-6 rounded-xl items-center justify-evenly'
     },
     {
       avatarUrl: '',
       fullName: 'RAKOTONANDRASANA Jean Pierre',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'bLeft bg-[#F0F0F0] flex flex-col m-4 rounded-xl items-center justify-evenly'
+      classes:'bLeft bg-[#F0F0F0] flex flex-col m-6 rounded-xl items-center justify-evenly'
     },
     {
       avatarUrl: '',
       fullName: 'RAKOTONANDRASANA Jean Pierre',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'bRight bg-[#F0F0F0] flex flex-col m-4 rounded-xl items-center justify-evenly'
+      classes:'bRight bg-[#F0F0F0] flex flex-col m-6 rounded-xl items-center justify-evenly'
     }
   ]
   return (
-    <div className='bg-[#0A0E26] h-[100vh] w-[100vw] flex flex-col items-center'>
-      <div className="h-full flex flex-col justify-between">
+    <div className='bg-[#0A0E26] h-[100vh] w-[100vw] flex flex-col general-content items-center'>
+      <div className="h-full flex flex-col justify-between  items-center w-full">
 
         {/* Texts section */}
-        <div className="h-[20%] flex flex-col justify-evenly">
+        <div className="h-[20%] flex flex-col justify-evenly titles">
           {/* Title of the Section */}
           <div className="text-center">
             <h2 className='font-semibold text-[2rem]'>MEET THE MEMBERS OF OUR TEAM</h2>
           </div>
 
           {/* Description below the title*/}
-          <div className="">
+          <div className="text-center">
             <p>We are a team of five Developers working in a different domains. The domains shown below represent only each member's specificity</p>
           </div>
         </div>
 
         {/* Cards section */}
-        <div className="h-[75%] w-full members-cards">
+        <div className="h-[75%] members-cards">
           {
             members.map((elem,key)=>(
               <div key={key} className={elem.classes}>
                 {/* Avatar */}
-                <div className="w-[8rem] h-[8rem] rounded-full bg-gray-600">
+                <div className="avatar w-[8rem] h-[8rem] rounded-full bg-gray-600">
 
                 </div>
 
                 {/* Abouts */}
                   {/* Name */}
-                  <div className="w-[60%] text-center">
-                    <h2 className='text-[#0A0E26] font-bold'>{elem.fullName}</h2>
+                  <div className="text-center flex justify-center max-w-[80%]">
+                    <h2 className='text-[#0A0E26] font-bold fnames'>{elem.fullName}</h2>
                   </div>
                   {/* Funtion */}
                   <div className="">
-                    <p className='text-[#BA38F2] font-bold'>{elem.domain}</p>
+                    <p className='text-[#BA38F2] font-bold domain'>{elem.domain}</p>
                   </div>
                 {/* Contants Link */}
-                <div className="flex flex-row w-full justify-center space-x-6">
-                  <div className="w-[3rem] h-[3rem] bg-slate-500 rounded-full">
+                <div className="flex flex-row w-full justify-center space-x-6 contacts">
+                  <div className="icons w-[3rem] h-[3rem] bg-slate-500 rounded-full">
 
                   </div>
-                  <div className="w-[3rem] h-[3rem] bg-slate-500 rounded-full">
+                  <div className="icons w-[3rem] h-[3rem] bg-slate-500 rounded-full">
 
                   </div>
-                  <div className="w-[3rem] h-[3rem] bg-slate-500 rounded-full">
+                  <div className="icons w-[3rem] h-[3rem] bg-slate-500 rounded-full">
 
                   </div>
                 </div>
