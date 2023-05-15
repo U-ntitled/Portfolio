e22f6d836d02f0461137d2f92f744d48dd493051
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-15 13:16:50 +0300

    correctio footer et project

--- Components/Footer/Footer.jsx
+++ Components/Footer/Footer.jsx
@@ -14,23 +14,23 @@ function Footer() {
         </div>
       </div>
       <div className="flex justify-center items-center w-full ">
-        <div className="flex justify-between items-start w-[80%] flex-wrap footer space-y-5">
+        <div className="flex justify-between items-start w-[85%] flex-wrap footer space-y-5">
           <div className="flex flex-col space-y-2  ">
             <h6 className='uppercase font-bold text-xl'>overview</h6>
-            <a className='a' href='#about'>About us</a>
-            <a className='a' href='#service'>Our services</a>
-            <a className='a' href='#project'>Our projects</a>
-            <a className='a' href='#team'>Our team</a>
+            <a className='a opacity-70' href='#about'>About us</a>
+            <a className='a opacity-70' href='#service'>Our services</a>
+            <a className='a opacity-70' href='#project'>Our projects</a>
+            <a className='a opacity-70' href='#team'>Our team</a>
           </div>
           <div className="flex flex-col space-y-2">
             <h6 className='uppercase font-bold text-xl'>Our Services</h6>
-            <a className='' href='#about'>UI/UX Design</a>
-            <a className='' href='#service'>Web Development</a>
-            <a className='' href='#project'>Mobile Development</a>
+            <a className='opacity-70' href='#about'>UI/UX Design</a>
+            <a className='opacity-70' href='#service'>Web Development</a>
+            <a className='opacity-70' href='#project'>Mobile Development</a>
           </div>
-          <div className="flex flex-col space-y-2 md:w-[40%] w-[100%]">
+          <div className="flex flex-col space-y-4 md:w-[40%] w-[100%]">
               <h2 className='text-4xl font-bold'>LET’ S MAKE YOUR <span className='text-[#37BB8A]'>VISION</span> INTO <span className='text-[#37BB8A]'>REALITY</span></h2>
-              <button className='bg-[#C525EA] flex justify-center items-center text-[#fff] py-3 w-[80%] btn '>SHARE US YOUR PROJECT</button>
+              <button className='bg-[#C525EA] flex justify-center items-center text-[#fff] py-2 w-[90%] btn '>SHARE US YOUR PROJECT</button>
           </div>
         </div>
       </div>
