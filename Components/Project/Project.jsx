39866a3caba38a6017b03fcee1064d8fc828611f
andrianiainafn <fanomezantsoanomenandrianiaina@gmail.com>
2023-05-17 07:48:05 +0300

    ajout d' animation

--- Components/Project/Project.jsx
+++ Components/Project/Project.jsx
@@ -26,7 +26,7 @@ function Project() {
   return (
     <div className='bg-[#171135] flex flex-col  items-center text-white  gap-y-3 space-section'>
       <h2 className="font-bold h2">Our Project</h2>
-      <p className="text-[#D9D9D9] text-center w-[100%] sx:[80%] opacity-95 pb-4" >Horem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.</p>
+      <p className="text-[#D9D9D9] text-center w-[100%] sx:[80%] opacity-95 pb-4" >Our portfolio showcases a collection of projects that we have successfully completed. Each of these projects represents our expertise and skillset in various domains. Whether it's graphic design, web development, content creation, or any other area of specialization, we have collaborated to deliver outstanding results. Explore our portfolio and discover the diversity and quality of our past projects.</p>
       <div className="grid grid-cols-1 gap-x-8 gap-y-8 lg:grid-cols-2 project-card ">
           {
             project.map((elem,key)=>(
--- Components/Team/Team.jsx
+++ Components/Team/Team.jsx
@@ -1,5 +1,7 @@
+'use client'
 import React from 'react'
 import './style.team.css'
+import {motion} from 'framer-motion'
 
 function Team() {
   const members = [
@@ -8,35 +10,41 @@ function Team() {
       fullName: 'RAZAFIMAMY Antonino Iraky Ny Avo',
       domain:'Backend Developper',
       contacts: ['','',''],
-      classes:'upLeft'
+      classes:'upLeft',
+      direction: true
     },
     {
       avatarUrl: '',
       fullName: 'ANDRIANIAINA Fanomezantsoa Nomena',
       domain:'MERN Stack, Mobile Developper',
       contacts: ['','',''],
-      classes:'center'
+      classes:'center', 
+      direction:false
+
     },
     {
       avatarUrl: '',
       fullName: 'RAKOTONDRANAIVO Gilbert Joyaux Wendi Anderson',
       domain:'Django, VueJs, Laravel Developper',
       contacts: ['','',''],
-      classes:'upRight'
+      classes:'upRight',
+      direction:true
     },
     {
       avatarUrl: '',
       fullName: 'NOMENJANAHARY Riry',
       domain:'Fullstack Developper',
       contacts: ['','',''],
-      classes:'bLeft'
+      classes:'bLeft',
+      direction:false
     },
     {
       avatarUrl: '',
       fullName: 'VONIARIMALALA Fiaro Miangaly',
       domain:'Junior Data Scientist',
       contacts: ['','',''],
-      classes:'bRight'
+      classes:'bRight',
+      direction:true
     }
   ]
   return (
@@ -60,7 +68,11 @@ function Team() {
         <div className="h-[82%] members-cards">
           {
             members.map((elem,key)=>(
-              <div key={key} className={elem.classes}>
+              <motion.div 
+              initial={{x: elem.direction ? -100 : 100,opacity:0}}
+              transition={{duration: 0.5 }}
+              whileInView={{x:0,opacity:1}}
+              key={key} className={elem.classes}>
                 {/* Avatar */}
                 <div className="avatar w-[8.5rem] h-[8.5rem] rounded-full bg-gray-600">
 
@@ -87,7 +99,7 @@ function Team() {
 
                   </div>
                 </div>
-              </div>
+              </motion.div>
             ))
           }
         </div>
