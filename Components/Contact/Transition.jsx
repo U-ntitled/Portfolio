bd1133ac09491ed1c46bedb859bd5edf99670282
PyWendi <rakotondranaivogilbert21@gmail.com>
2023-05-22 14:12:09 +0300

    Edit contact text

--- Components/Contact/Transition.jsx
+++ Components/Contact/Transition.jsx
@@ -9,7 +9,8 @@ function Transition() {
                 <h2 className={`${styles.h2_low} h2`}>LET'S CREATE <span className={`${styles.green}`}>SOMETHING AMAZING</span> TOGETHER</h2>
             </div>
             <p className={styles.text}>
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam eveniet nobis perspiciatis ab ipsa. 
+                Ready to take the plunge into the digital dimension? Have questions, ideas, or partnerships in mind? Our contact section is just a click away. 
+                Drop us a line, and our talented teams are here to simplify the complex world of technology and provide you with tailored solutions that meet your specific needs.
             </p>
 
             <div className={styles.purple_button}>
